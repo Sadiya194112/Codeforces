c352176d013976e526a440c18678024f19a655c6
Sadiya194112 <59763617+Sadiya194112@users.noreply.github.com>
2023-04-25 00:01:17 +0600

    Math

---
+++ 1822C.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define int long long
+#define endl "\n"
+using namespace std;
+void ac(){
+    int n,ans;  cin>>n;
+    ans = (n*n)+(n*2)+2;
+    cout<<ans<<endl;
+}
+signed main(){
+    int tc;     cin>>tc;
+    while(tc--){
+        ac();
+    }
+    return 0;
+}
